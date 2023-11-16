255da81d482c167296289ee8091e31df77b1fb36
cuadradot <cuadradot@predictia.es>
2023-11-16 21:01:54 +0100

    test_persistencia updated

--- tests/test_persistencia.py
+++ tests/test_persistencia.py
@@ -10,3 +10,16 @@ def test_persistencia():
     ]
     for pedido in pedidos:
         guardar_pedido(pedido["nombre"], pedido["apellidos"])
+
+
+def test_guardar_pedido():
+    """
+    Prueba general
+    """
+    with open("../pedidos/pedidos.txt", "w+", encoding="utf-8") as file:
+        guardar_pedido("Pedro", "Gil de Diego")
+        guardar_pedido("Michael", "Jordan")
+        firstline = file.readline()
+        secondline = file.readline()
+    assert firstline == "-Pedro Gil de Diego\n"
+    assert secondline == "-Michael Jordan\n"
